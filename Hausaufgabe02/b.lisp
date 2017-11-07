88ab334c2f6bbc74fbe750d2018a25e9aa3c58c3
Marius Hobbhahn <marius.hobbhahn@student.uni-tuebingen.de>
2017-11-08 00:47:49 +0100

    finished 3d

--- Hausaufgabe02/b.lisp
+++ Hausaufgabe02/b.lisp
@@ -10,7 +10,7 @@
 	(if (null a)
 		'()
 		(cons (+ (car a) (car b)) (addElementwise (cdr a) (cdr b)))))
-		
+
 (defun addSort (a b)
 	"adds lists a and b element-wise, then sorts the result and removes duplicates"
 	(remove-duplicates (sort (addElementwise a b) #'<)))
@@ -23,7 +23,7 @@
 		(list level)
 		(append 	(if (not (null (second tree))) (searchTreeCount a (second tree) (+ level 1)))
 						(if (not (null (third tree))) (searchTreeCount a (third tree) (+ level 1))))))
-	
+
 (defun searchTree (a tree)
 	"looks for value a in the binary tree; if a is in the tree it returns its level - otherwise NIL"
 	(if (= (car tree) a)
@@ -37,37 +37,58 @@
 	(cond	((null tree) 				nil)
 				((atom (car tree)) 	(cons (car tree) (flatten (cdr tree))))
 				(t 								(append (flatten (car tree)) (flatten (cdr tree))))))
-				
-(defun remove-nil (liste)
+
+(defun removeNIL (liste)
 	"removes unnecessary nil from the list"
 	(if (and (null (car liste)) (null (cdr liste)))
 		'()
 		(if (null (car liste))
-			(remove-nil (cdr liste))
-			(cons (car liste) (remove-nil (cdr liste))))))
+			(removeNIL (cdr liste))
+			(cons (car liste) (removeNIL (cdr liste))))))
 
-(defun remove-element (liste a)
-	"removes given element a from list l"
+(defun removeElement (liste a)
+	"removes given element a from list"
 	(if (null (car liste))
 		'()
 		(if (= (car liste) a)
-			(remove-element (cdr liste) a)
-			(cons (car liste) (remove-element (cdr liste) a)))))
-	
+			(removeElement (cdr liste) a)
+			(cons (car liste) (removeElement (cdr liste) a)))))
+
+(defun insertIntoTree (element tree)
+	"insert given element into pre-existing binary (search-)tree"
+	(if (null (car tree))
+		(list element nil nil)
+		(if (< element (car tree))
+			(list (car tree) (insertIntoTree element (second tree)) (third tree))
+			(list (car tree) (second tree) (insertIntoTree element (third tree))))))
+
+(defun buildTreeHelper (liste tree)
+	"build a tree from a given list"
+	(if (null (car liste))
+		tree
+		(buildTreeHelper (cdr liste) (insertIntoTree (car liste) tree))))
+
 (defun buildTree (liste)
 	"build a tree from a given list"
-	
-	)
-
+	(buildTreeHelper liste '(nil nil nil)))
 
 (defun deleteTree (a tree)
 	"deletes all instances of value a from binary tree"
 	(if (null (searchTree a tree))
 		tree
-		(buildTree (remove-element (remove-nil (flatten tree)) a))))
+		(buildTree (removeElement (removeNIL (flatten tree)) a))))
 ;
 ; 	(d) mapFilter
 ;
 (defun mapFilter (a f)
 	"applies function f on values of list a. Only not NIL values will be returned"
-	(mapcar #'f a))
+	(if (null a)
+		'()
+		(if (not (null (funcall f (car a))))
+			(cons (funcall f (car a)) (mapFilter (cdr a) f))
+			(mapFilter (cdr a) f)
+			)))
+
+;(write ( mapFilter '(1 2 5 15 24 13 14)
+;	( lambda ( x )
+;		( if ( evenp x ) (1+ x ) nil ))))
